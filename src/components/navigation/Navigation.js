4004774128f6acaec0f0161cbcd7e78660c4e78b
Amari Charles <amarichar@gmail.com>
2022-12-15 19:00:32 -0500

    test

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -13,13 +13,13 @@ export default function Navigation() {
     <div>
       <Router basename="/Portfolio">
         <Routes>
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/projects" element={<Projects />} />
-          <Route exact path="/credits" element={<Credits />} />
-          <Route exact path="/royal-road" element={<RoyalRoad />} />
-          <Route exact path="personas" element={<Personas />} />
-          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="/royal-road" element={<RoyalRoad />} />
+          <Route path="personas" element={<Personas />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
